94ac9e2c39459efc4c6165d4f5bf246fea95eb8a
thealfit <thealfit@yandex.ru>
2026-05-25 11:44:44 +0300

    add models and interact with YOLO

---
+++ detect/neyro_main.py
@@ -0,0 +1,79 @@
+import cv2
+import pandas as pd
+from ultralytics import YOLO
+from tracker import*
+import cvzone
+import numpy as np
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+model_path = BASE_DIR / "models" / "model.pt" #указываем версию YOLO, которую будем использовать
+
+model = YOLO(model_path)
+
+#def RGB(event, x, y):
+#    if event == cv2.EVENT_MOUSEMOVE:
+#        point = [x, y]
+#        print(point)
+  
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+cap = cv2.VideoCapture('test_eclair_video.mp4')
+
+my_file = open("find_object.txt", "r")
+data = my_file.read()
+class_list = data.split("\n")
+
+tracker = Tracker()
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = 1020
+height = 500
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = cv2.resize(frame, (width, height))
+
+    results = model.predict(frame)
+    a = results[0].boxes.data
+    px = pd.DataFrame(a.cpu().numpy()).astype("float")
+
+    list = []
+
+    for index, row in px.iterrows():
+        x1 = int(row[0])
+        y1 = int(row[1])
+        x2 = int(row[2])
+        y2 = int(row[3])
+        d = int(row[5])
+
+        c = class_list[d]
+
+        if 'eclair' in c:
+            list.append([x1, y1, x2, y2])
+
+    bbox_idx = tracker.update(list)
+
+    for bbox in bbox_idx:
+        x3, y3, x4, y4, id = bbox
+        cv2.circle(frame, (x4, y4), 4, (100, 0, 255), -1)
+        cv2.rectangle(frame, (x3, y3), (x4, y4), (255, 255, 255), 2)
+        cvzone.putTextRect(frame, f'{id}', (x3, y3), 1, 1)
+
+    out.write(frame)
+
+    cv2.imshow("RGB", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
