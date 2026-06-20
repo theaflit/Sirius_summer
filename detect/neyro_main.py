e1d9a77f7b209ff4089ed92a5a5132db2f21ba2c
thealfit <thealfit@yandex.ru>
2026-06-20 17:34:00 +0300

    refactor: move video processing logic to a function

--- detect/neyro_main.py
+++ detect/neyro_main.py
@@ -5,70 +5,79 @@ from tracker import*
 import cvzone
 import numpy as np
 from pathlib import Path
-from test_gpu import test_cuda
+from test_gpu import check_device
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-model_path = BASE_DIR / "models" / "best.pt" #указываем версию YOLO, которую будем использовать
+def launch_yolo(model_name: str, 
+                device: str, 
+                video: bool, 
+                video_name: str | None = None):
 
-model = YOLO(model_path)
-if test_cuda():
-    model.to("cuda")
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    model_path = BASE_DIR / "models" / model_name # указываем версию YOLO, которую будем использовать
 
-#def RGB(event, x, y):
-#    if event == cv2.EVENT_MOUSEMOVE:
-#        point = [x, y]
-#        print(point)
-  
-# cv2.namedWindow('RGB')
-# cv2.setMouseCallback('RGB', RGB)
+    model = YOLO(model_path)
+    device = check_device(device)
+    model.to(device)
 
-cap = cv2.VideoCapture(BASE_DIR / "detect" / 'test_eclair_video.mp4')
+    #def RGB(event, x, y):
+    #    if event == cv2.EVENT_MOUSEMOVE:
+    #        point = [x, y]
+    #        print(point)
+    
+    # cv2.namedWindow('RGB')
+    # cv2.setMouseCallback('RGB', RGB)
 
-my_file = open(BASE_DIR / "detect" / "find_object.txt", "r")
-data = my_file.read()
-class_list = data.split("\n")
+    source = BASE_DIR / "detect" / video_name if video else 0
+    cap = cv2.VideoCapture(source)
 
-tracker = Tracker()
+    my_file = open(BASE_DIR / "detect" / "find_object.txt", "r")
+    data = my_file.read()
+    class_detections = data.split("\n")
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    tracker = Tracker()
 
-    frame = cv2.resize(frame, (1020, 500))
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    results = model.predict(frame, verbose=False)
-    a = results[0].boxes.data
-    px = pd.DataFrame(a.cpu().numpy()).astype("float")
+        frame = cv2.resize(frame, (1020, 500))
 
-    list = []
+        results = model.predict(frame, verbose=False)
+        a = results[0].boxes.data
+        px = pd.DataFrame(a.cpu().numpy()).astype("float")
 
-    for index, row in px.iterrows():
-        x1 = int(row[0])
-        y1 = int(row[1])
-        x2 = int(row[2])
-        y2 = int(row[3])
-        d = int(row[5])
+        detections = []
 
-        c = class_list[d]
+        for index, row in px.iterrows():
+            x1 = int(row[0])
+            y1 = int(row[1])
+            x2 = int(row[2])
+            y2 = int(row[3])
+            d = int(row[5])
 
-        if 'eclair' in c:
-            list.append([x1, y1, x2, y2])
+            c = class_detections[d]
 
-    bbox_idx = tracker.update(list)
+            if 'eclair' in c:
+                detections.append([x1, y1, x2, y2])
 
-    for bbox in bbox_idx:
-        x3, y3, x4, y4, id = bbox
-        cv2.circle(frame, (x4, y4), 4, (100, 0, 255), -1)
-        cv2.rectangle(frame, (x3, y3), (x4, y4), (255, 255, 255), 2)
-        cvzone.putTextRect(frame, f'{id}', (x3, y3), 1, 1)
+        bbox_idx = tracker.update(detections)
 
-    cv2.imshow("RGB", frame)
+        for bbox in bbox_idx:
+            x3, y3, x4, y4, id = bbox
+            cv2.circle(frame, (x4, y4), 4, (100, 0, 255), -1)
+            cv2.rectangle(frame, (x3, y3), (x4, y4), (255, 255, 255), 2)
+            cvzone.putTextRect(frame, f'{id}', (x3, y3), 1, 1)
 
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+        cv2.imshow("RGB", frame)
 
-cap.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
 
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    launch_yolo("best.pt", 'cuda', True, 'test_eclair_video.mp4')
--- detect/test_gpu.py
+++ detect/test_gpu.py
@@ -1,13 +1,30 @@
 import torch
 
+def check_device(device: str):
+    device = device.lower()
+
+    if device not in ('gpu', 'cuda', 'cpu'):
+        raise ValueError("device должен быть GPU или CPU")
+
+    if device == 'cpu':
+        return 'cpu'
+
+    return 'cuda' if test_cuda() else 'cpu'
+
 def test_cuda():
     if torch.cuda.is_available():
-        print("Доступный GPU:", torch.cuda.get_device_name(0))
+        device = torch.cuda.get_device_name(0)
+        
+        print("Доступный GPU:", green_text(device))
         print("CUDA версия:", torch.version.cuda)
 
         return True
     
+    print("GPU не найден, поэтому будет использова CPU")
     return False
+
+def green_text(text):
+    return '\033[92m' + text + '\033[0m'
 
 if __name__ == "__main__":
     test_cuda()
